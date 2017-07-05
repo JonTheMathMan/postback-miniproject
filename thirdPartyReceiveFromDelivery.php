61f778af671bd585b722668d77ee120343c500a7
jon <jon@playgroundG>
2017-07-05 18:48:46 +0000

    added handling for post

--- thirdPartyReceiveFromDelivery.php
+++ thirdPartyReceiveFromDelivery.php
@@ -1,8 +1,19 @@
 <html>
 <body>
 	<?php
-		echo '<h1>'.$_GET["title"].'</h1>';
-    		echo '<img src='.$_GET["image"].'/>';
+		if($_GET["title"]!="" && $_GET["image"]!="") {
+			echo '<h1>'.$_GET["title"].'</h1>';
+    			echo '<img src='.$_GET["image"].'/>';
+		}
+
+		if($_POST!=null) {
+			foreach($_POST as $key => $value) {
+				echo "<h1>$key</h1>";
+				echo "<p>$value</p>";
+			}
+		unset($key);
+		unset($value);
+		}
 	?>
 </body>
 </html>
